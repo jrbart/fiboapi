8de3901a10f327a847199a5d11b3f9c7c86dc3a4
Randy Bartels <bartels@jr.reno.nv.us>
2014-05-26 21:19:59 -0500

    Add test fibo(3).  Factored in real algorithm.

--- mymath/fibo.py
+++ mymath/fibo.py
@@ -1,7 +1,12 @@
 #!/usr/env python2
 
 def fibo(n):
-    return [0]
+    result = [0]
+    a, b = 0, 1
+    for i in range(n-1):
+        result.append(b)
+        a, b = b, a+b
+    return result
 
 if __name__ == "__main__":
     pass
--- test_fibo.py
+++ test_fibo.py
@@ -6,6 +6,12 @@ class TestFibo(unittest.TestCase):
     def test_fib1(self):
         self.assertEqual(fibo(1),[0])
 
+    def test_fib2(self):
+        self.assertEqual(fibo(2),[0, 1])
+
+    def test_fib3(self):
+        self.assertEqual(fibo(3),[0, 1, 1])
+
 if __name__ == "__main__":
     unittest.main()
 
